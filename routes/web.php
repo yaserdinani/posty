eda4117d9fb2a6ee51b2904efa8cca5843c86fc0
yaserrezaeidinani <yaserdinani1999@gmail.com>
2021-08-30 19:34:39 +0430

    + add register feature + add register view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/posts', function () {
     return view('posts.index');
 });
+
+Route::get('/register',[App\Http\Controllers\Auth\RegisterController::class,'index'])->name('register');
+Route::post('/register',[App\Http\Controllers\Auth\RegisterController::class,'store']);
